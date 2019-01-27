7eac8efd279cfc640e98ccd266c3475ae58bc8e5
Erik Vullings <erik.vullings@gmail.com>
2019-01-27 11:53:53 +0100

    Closes #1. ISelectOptions no longer extends IInputOptions

--- packages/lib/src/select.ts
+++ packages/lib/src/select.ts
@@ -1,6 +1,5 @@
 import m, { FactoryComponent } from 'mithril';
-import { uniqueId, toDottedClassList } from './utils';
-import { IInputOptions } from './input-options';
+import { toDottedClassList, isNumeric } from './utils';
 import { Label, HelperText } from './label';
 
 export interface ISelectOption {
@@ -9,24 +8,55 @@ export interface ISelectOption {
   disabled?: boolean;
 }
 
-export interface ISelectOptions extends Partial<M.FormSelectOptions>, IInputOptions {
+export interface ISelectOptions extends Partial<M.FormSelectOptions> {
+  /** Options to select from */
   options: ISelectOption[];
-  onchange: (value?: string | number | string[] | number[]) => void;
-  checkedId?: string | number | string[] | number[];
+  /** Called when the value is changed, either contains a single or all selected (checked) ids */
+  onchange: (value?: string | number | Array<string | number>) => void;
+  /** Selected id or ids (in case of multiple options) */
+  checkedId?: string | number | Array<string | number>;
+  /** Select a single option or multiple options */
   multiple?: boolean;
+  /** Optional label. */
+  label?: string;
+  /** Optional ID. */
+  id?: string;
+  /** Unique key for use of the element in an array. */
+  key?: string | number;
+  /** Add a a placeholder to the input field. */
+  placeholder?: string;
+  /** Add a description underneath the input field. */
+  helperText?: string;
+  /** Uses Materialize icons as a prefix or postfix. */
+  iconName?: string;
+  /** Sets the input field to disabled. */
+  disabled?: boolean;
+  /** Optional style information. */
+  style?: string;
+  /** If true, break to a new row */
+  newRow?: boolean;
+  /** Classes that you wish to attach to the content, e.g. "col s12 m6 l4 xl3" to specify the size. */
+  contentClass?: string;
+  /**
+   * If true, add a mandatory * after the label (if any),
+   * and add the required and aria-required attributes to the input element.
+   */
+  isMandatory?: boolean;
+  /** Add the required and aria-required attributes to the input element */
+  required?: boolean;
 }
 
 /** Component to select from a list of values in a dropdowns */
 export const Select: FactoryComponent<ISelectOptions> = () => {
   const state = {
-    id: uniqueId(),
     instance: undefined as M.FormSelect | undefined,
   };
-  const isSelected = <T extends number | string>(id: T, checkedId?: T | T[], selected = false) =>
-    selected || (checkedId instanceof Array ? checkedId.indexOf(id) >= 0 : checkedId === id);
+  const isSelected = (id?: string | number, checkedId?: string | number | Array<string | number>, selected = false) =>
+    selected || (id && checkedId instanceof Array ? checkedId.indexOf(id) >= 0 : checkedId === id);
   return {
     view: ({
       attrs: {
+        id,
         checkedId,
         newRow,
         contentClass,
@@ -40,9 +70,9 @@ export const Select: FactoryComponent<ISelectOptions> = () => {
         iconName,
       },
     }) => {
-      const id = state.id;
       const clear = newRow ? '.clear' : '';
-      const validSelection = options.filter(o => isSelected(o.id || o.label, checkedId)).length > 0;
+      const validSelection = options.filter(o => isSelected(o.id, checkedId)).length > 0;
+
       return m(`.input-field.select-space${clear}${toDottedClassList(contentClass)}`, [
         iconName ? m('i.material-icons.prefix', iconName) : undefined,
         m(
@@ -58,10 +88,12 @@ export const Select: FactoryComponent<ISelectOptions> = () => {
               ? (e: Event) => {
                   if (multiple) {
                     const values = state.instance && state.instance.getSelectedValues();
-                    onchange(values);
+                    const v = values ? values.map(n => isNumeric(n) ? +n : n) : undefined;
+                    onchange(v as Array<string | number> | undefined);
                   } else if (e && e.currentTarget) {
                     const b = e.currentTarget as HTMLButtonElement;
-                    onchange(b.value);
+                    const v = isNumeric(b.value) ? +b.value as number : b.value as string;
+                    onchange(v as string | number);
                   }
                 }
               : undefined,
@@ -70,7 +102,7 @@ export const Select: FactoryComponent<ISelectOptions> = () => {
           options.map((o, i) =>
             m(
               `option[value=${o.id}]${o.disabled ? '[disabled]' : ''}${
-                isSelected(o.id || o.label, checkedId, i === 0 && !validSelection && !placeholder) ? '[selected]' : ''
+                isSelected(o.id, checkedId, i === 0 && !validSelection && !placeholder) ? '[selected]' : ''
               }`,
               o.label.replace('&amp;', '&')
             )
