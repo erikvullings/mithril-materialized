26d5e7c31d6454cd9974dfb56a0b2fbd13e9829c
Erik Vullings <erik.vullings@gmail.com>
2019-03-01 15:27:59 +0100

    Added documentation using typedoc

--- packages/example/src/components/home/home-page.ts
+++ packages/example/src/components/home/home-page.ts
@@ -12,10 +12,15 @@ export const HomePage = () => ({
           m(
             'p',
             `I like Mithril, and I also like materialize-css. However, to create some materialized components
-          is a bit cumbersome as it requires a lot of HTML elements, and a specific nesting which can easily go
+          is a bit cumbersome as it requires a lot of HTML elements and a specific nesting which can easily go
           wrong. For that reason, the mithril-materialized library provides you with several ready-made
           Mithril components, so you can easily use them in your own application.`
           ),
+          m('p', [
+            'You can check out the API documentation ',
+            m('a[href="https://erikvullings.github.io/mithril-materialized/typedoc/index.html"]', 'here'),
+            '.',
+          ]),
           m('h3', 'Installation'),
           m('p', 'First, you need to install the required packages:'),
           m(CodeBlock, {
