bc5fc8b647e372c3c2aa5a8120ab4bc1a6262177
Erik Vullings <erik.vullings@gmail.com>
2019-03-04 12:32:35 +0100

    FIX tslint issue

--- packages/example/src/components/timeline/timeline-page.ts
+++ packages/example/src/components/timeline/timeline-page.ts
@@ -35,7 +35,7 @@ export const TimelinePage = () => {
               title: 'Test a long text',
               iconName: 'play_arrow',
               datetime: new Date(2019, 2, 3, 9, 30, 0),
-              content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus maximus erat, 
+              content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus maximus erat,
               vitae placerat nisl blandit tincidunt. Vestibulum libero turpis, bibendum sit amet rutrum a,
               malesuada at diam. Praesent id dignissim ligula. Donec nec finibus lectus. Curabitur in
               sollicitudin sem. Nulla neque est, elementum et lectus ut, luctus elementum metus.`,
