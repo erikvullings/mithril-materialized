fbf518c331abe148ee5742e7bf9e474034d2e5c0
Erik Vullings <erik.vullings@gmail.com>
2019-06-01 16:22:29 +0200

    id can be string|number, label with less spacing and smaller

--- packages/lib/src/radio.ts
+++ packages/lib/src/radio.ts
@@ -1,24 +1,29 @@
 import m, { FactoryComponent, Attributes } from 'mithril';
 import { uniqueId } from './utils';
+import { IInputOption } from './option';
+import { Label } from './label';
 
 export interface IRadioButtons extends Attributes {
-  label: string;
-  options: Array<{ id: string; label: string }>;
-  onchange: (id: string) => void;
-  checkedId?: string;
+  label?: string;
+  options: IInputOption[];
+  onchange: (id: string | number) => void;
+  checkedId?: string | number;
   description?: string;
   newRow?: boolean;
+  isMandatory?: boolean;
 }
 
 /** Component to show a list of radio buttons, from which you can choose one. */
 export const RadioButtons: FactoryComponent<IRadioButtons> = () => {
   const state = { id: uniqueId() };
   return {
-    view: ({ attrs: { newRow, className = 'col s12', label, description, onchange, options, checkedId } }) => {
+    view: ({
+      attrs: { newRow, className = 'col s12', label = '', description, onchange, options, checkedId, isMandatory },
+    }) => {
       const groupId = state.id;
       const clear = newRow ? '.clear' : '';
-      return m(`.input-field${clear}`, { className }, [
-        m('h4', m.trust(label)),
+      return m(`div${clear}`, { className }, [
+        m('h6', m(Label, { label, isMandatory })),
         description ? m('p.helper-text', m.trust(description)) : '',
         ...options.map(r =>
           m(RadioButton, {
@@ -34,9 +39,9 @@ export const RadioButtons: FactoryComponent<IRadioButtons> = () => {
 };
 
 export interface IRadioButton extends Attributes {
-  id: string;
+  id: string | number;
   checked?: boolean;
-  onchange: (id: string) => void;
+  onchange: (id: string | number) => void;
   label: string;
   groupId: string;
   disabled?: boolean;
@@ -45,7 +50,8 @@ export interface IRadioButton extends Attributes {
 export const RadioButton: FactoryComponent<IRadioButton> = () => ({
   view: ({ attrs: { id, groupId, label, onchange, className = 'col s12', checked } }) => {
     return m(
-      `div`, { className },
+      `div`,
+      { className },
       m('label', [
         m(`input[type=radio][tabindex=0][name=${groupId}]${checked ? '[checked=checked]' : ''}`, {
           onclick: onchange ? () => onchange(id) : undefined,
