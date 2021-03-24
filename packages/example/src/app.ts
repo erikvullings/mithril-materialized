97ee4c6556de9f01e38ef25052b63a5424c5bfed
Erik Vullings <erik.vullings@gmail.com>
2021-03-24 18:39:18 +0100

    Import css of library

--- packages/example/src/app.ts
+++ packages/example/src/app.ts
@@ -1,5 +1,6 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'material-icons/iconfont/material-icons.css';
+import 'mithril-materialized/dist/index.css';
 import m from 'mithril';
 import { dashboardSvc } from './services/dashboard-service';
 
