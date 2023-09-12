f055a35fc37ddba2e0d04e0660ab317216b60bcf
Erik Vullings <erik.vullings@tno.nl>
2023-09-12 17:38:35 +0200

    Explicitly import materialize js. Only load filled font

--- packages/example/src/app.ts
+++ packages/example/src/app.ts
@@ -1,7 +1,10 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import 'material-icons/iconfont/material-icons.css';
+import 'materialize-css/dist/js/materialize.min.js';
+import 'material-icons/iconfont/filled.css';
 import 'mithril-materialized/index.css';
 import m from 'mithril';
 import { dashboardSvc } from './services/dashboard-service';
+// import '@materializecss/materialize/dist/css/materialize.min.css';
+// import '/home/erik/dev/mithril-materialized/node_modules/.pnpm/@materializecss+materialize@2.0.1-alpha/node_modules/@materializecss/materialize/dist/css/materialize.min.css';
 
 m.route(document.body, dashboardSvc.defaultRoute, dashboardSvc.routingTable);
