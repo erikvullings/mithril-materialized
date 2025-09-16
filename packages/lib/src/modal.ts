e59f42629e9cd6f1663272e974d9d3584b687475
Erik Vullings <erik.vullings@gmail.com>
2025-09-16 23:12:56 +0200

    fix: modal should not block event handlers for children

--- packages/lib/src/modal.ts
+++ packages/lib/src/modal.ts
@@ -195,7 +195,7 @@ export const ModalPanel: FactoryComponent<ModalAttrs> = () => {
               boxShadow:
                 '0 24px 38px 3px rgba(0,0,0,0.14), 0 9px 46px 8px rgba(0,0,0,0.12), 0 11px 15px -7px rgba(0,0,0,0.20)',
             },
-            onclick: (e: Event) => e.stopPropagation(), // Prevent backdrop click when clicking inside modal
+            // onclick: (e: Event) => e.stopPropagation(), // Prevent backdrop click when clicking inside modal
           },
           [
             // Close button
