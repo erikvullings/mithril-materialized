196dc5bf9b12b9df406d59da23688cff2db8bff7
Erik Vullings <erik.vullings@tno.nl>
2026-04-07 22:58:46 +0200

    chore: update GitHub Actions to use latest action versions and improve Jest config

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -3,33 +3,27 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src', '<rootDir>/__tests__'],
-  testMatch: [
-    '**/__tests__/**/*.test.ts',
-    '**/__tests__/**/*.test.tsx',
-    '**/*.test.ts',
-    '**/*.test.tsx'
-  ],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx', '**/*.test.ts', '**/*.test.tsx'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: {
-        target: 'es2017',
-        lib: ['dom', 'es2017', 'es2018']
-      }
-    }],
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          target: 'es2017',
+          lib: ['dom', 'es2017', 'es2018'],
+          moduleResolution: 'node16',
+        },
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   setupFilesAfterEnv: ['<rootDir>/__tests__/test-setup.ts'],
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-    '!__tests__/test-setup.ts',
-    '!src/index.ts'
-  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!__tests__/test-setup.ts', '!src/index.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   testTimeout: 10000,
-  verbose: true
+  verbose: true,
 };
